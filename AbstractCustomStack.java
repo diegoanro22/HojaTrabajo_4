7684b1caceea1bf5ab46548c8e60adcd3a94a8e7
GenserCatalan <gensercatalanandre@gmail.com>
2024-02-20 19:04:33 -0600

    Se agrego la clase AbstractCustomStack

---
+++ AbstractCustomStack.java
@@ -0,0 +1,8 @@
+public abstract class AbstractCustomStack<T> {
+    public abstract void push(T item);
+    public abstract T pop();
+    public abstract T peek();
+    public abstract boolean isEmpty();
+}
+
+
