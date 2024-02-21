ba5cd76891481b81fc94ff22b7c9c3159f29f1f2
GenserCatalan <gensercatalanandre@gmail.com>
2024-02-20 19:18:08 -0600

    Se agrego la clase de implementacion vector

---
+++ Vector.java
@@ -0,0 +1,24 @@
+import java.util.Vector;
+
+public class VectorPila<T> extends AbstractCustomStack<T> {
+    private CustomStack<T> stack;
+
+    public VectorPila() {
+        stack = new CustomStack<>();
+    }
+
+    @Override
+    public void push(T item) {
+        stack.push(item);
+    }
+
+    @Override
+    public T pop() {
+        return stack.pop();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return stack.isEmpty();
+    }
+}
