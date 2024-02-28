5f2895bb1505d56310a81dfd134ec7b114ba41bd
GenserCatalan <gensercatalanandre@gmail.com>
2024-02-27 22:09:54 -0600

    Se arreglaron los errores en los test

--- Test/TestImplementaciones.java
+++ Test/TestImplementaciones.java
@@ -5,7 +5,7 @@ public class TestImplementaciones {
 
     //Test de la implementacion de la clase Arraylist
     @Test
-    public void TestArrayListPila() {
+    public void testArrayListPila() {
         StackInterface<Integer> pila = new ArrayListStack<>();
         pila.push(1);
         pila.push(2);
@@ -19,7 +19,7 @@ public class TestImplementaciones {
 
     //Test de la implementacion de la clase Vector
     @Test
-    public void TestVectorPila() {
+    public void testVectorPila() {
         VectorStack<Integer> pila = new VectorStack<>();
         pila.push(1);
         pila.push(2);
@@ -33,30 +33,31 @@ public class TestImplementaciones {
 
     //Test de la implementacion de la clase de listas dobles
     @Test
-    public void TestDoublyLinkedList() {
+    public void testDoublyLinkedList() {
         DoublyLinkedList<Integer> list = new DoublyLinkedList<>();
         assertTrue(list.isEmpty());
 
         list.add(1);
         assertFalse(list.isEmpty());
-        assertEquals(Integer.valueOf(1), list.getHead().getValue());
-        assertEquals(Integer.valueOf(1), list.getLast().getValue());
+        assertEquals(Integer.valueOf(1), list.getHead());
+        assertEquals(Integer.valueOf(1), list.getLast());
 
         list.add(2);
-        assertEquals(Integer.valueOf(2), list.getLast().getValue());
+        assertEquals(Integer.valueOf(1), list.getHead());
+        assertEquals(Integer.valueOf(2), list.getLast());
     }
 
     //Test de la implementacion de la clase de lista unica
     @Test
-    public void TestSinglyLinkedList() {
+    public void testSinglyLinkedList() {
         SinglyLinkedList<Integer> list = new SinglyLinkedList<>();
         list.add(1);
         list.add(2);
         list.add(3);
 
-        assertEquals(Integer.valueOf(1), list.get(0).getValue());
-        assertEquals(Integer.valueOf(2), list.get(1).getValue());
-        assertEquals(Integer.valueOf(3), list.get(2).getValue());
+        assertEquals(Integer.valueOf(1), list.get(0));
+        assertEquals(Integer.valueOf(2), list.get(1));
+        assertEquals(Integer.valueOf(3), list.get(2));
         assertNull(list.get(3));
     }
 }
